ee18bd0d7e2364a869ccc30ad817005ccaed654b
KeadenM <keadenmeacham@gmail.com>
2023-07-24 18:24:55 -0600

    created prompts

--- index.js
+++ index.js
@@ -2,3 +2,27 @@ const inquirer = require('inquirer');
 const jest = require('jest');
 const fs = require('fs');
 const {Shape, Triangle, Square, Circle} = require('.lib/shape');
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: "letters",
+            message: "Which 3 letters would you like to use?"
+        },
+        {
+            type: 'input',
+            name: "textColor",
+            message: "What color would you like the text to be?"
+        },
+        {
+            type: "lists",
+            name: "shape",
+            message: "Which shape would you like to use?",
+            choices: ["Triangle", "Square", "Circle"],
+        },
+        {
+            type: "input",
+            name: "shapeColor",
+            message: "What color would you like the shape to be?",
+        },
+    ])
