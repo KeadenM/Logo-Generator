fce3b009e0ca0f06144c80b0f4ccd5905732dfac
KeadenM <keadenmeacham@gmail.com>
2023-07-24 18:34:38 -0600

    created all shapes

--- index.js
+++ index.js
@@ -26,3 +26,29 @@ inquirer
             message: "What color would you like the shape to be?",
         },
     ])
+    .then((data) => {
+        const shape =data.shape;
+        if(shape =="Triangle") {
+            const triangle = new Triangle(data.letters, data.textColor, data.shapeColor);
+            console.log(triangle)
+            fs.writeFile("examples/triangle.svg", triangle.render(), (err) => {
+                err ? console.log(err) : console.log("Success!");
+        });
+    } else if (shape == "Square") {
+        const square = new Square(data.letters, data.textColor, data.shapeColor);
+        console.log(square)
+        fs.writeFile("examples/square.svg", square.render(), (err) => {
+            err ? console.log(err) : console.log("Success!");
+        });
+    });
+     else if (shape == "Circle") {
+        const circle = new Circle(data.letters, data.textColor, data.shapeColor);
+        console.log(circle)
+        fs.writeFile("examples/circle.svg", circle.render(), (err) => {
+            err ? console.log(err) : console.log("Success!");
+        });
+    }
+);
+};
+
+init();
